9da5f08fc817a7bb6e8acbe5d27322af1267e4ff
Jeffer Zhang <zhangjeffer@gmail.com>
2020-03-02 21:51:06 -0500

    90% done. To-do: functionality to add videos and users. clean up nav bars to include backs and signouts. clean up upload area.

---
+++ app.py
@@ -0,0 +1,96 @@
+import glob, os
+import json
+
+current_user = "demo"
+
+user_pass = {
+   "demo": "password"
+}
+
+deadlift = {
+   "demo": {"02/20/20": ["5.mov", "395lb x 3"],
+            "02/10/20": ["4.mov", "495lb x 2"],
+            "02/01/20": ["3.mov", "455lb x 1"],
+            "01/20/20": ["2.mov", "545lb x 1"],
+            "12/02/19": ["1.mov", "410lb x 3"]
+   }
+}
+
+squat = {
+   "demo": {"02/20/20": ["10.mov", "455lb x 1"],
+            "02/10/20": ["9.mov", "355lb x 4"],
+            "02/01/20": ["8.mov", "405lb x 2"],
+            "01/20/20": ["7.mov", "295lb x 4"],
+            "12/02/19": ["6.mov", "340lb x 4"]
+   }
+}
+
+bench = {
+   "demo": {"02/20/20": ["15.mov", "225lb x 4"],
+            "02/10/20": ["14.mov", "235lb x 3"],
+            "02/01/20": ["13.mov", "220lb x 4"],
+            "01/20/20": ["12.mov", "245lb x 3"],
+            "12/02/19": ["11.mov", "225lb x 4"]
+   }
+}
+
+
+def delete_vid(mov):
+   squat_dict = squat.get(current_user)
+   bench_dict = bench.get(current_user)
+   deadlift_dict = deadlift.get(current_user)
+   squat_bool = False
+   bench_bool = False
+   deadlift_bool = False
+   delete_date = None
+
+   for key,val in squat_dict.items():
+      if (val[0] == mov):
+         delete_date = key
+         squat_bool = True
+
+   for key,val in bench_dict.items():
+      if (val[0] == mov):
+         delete_date = key
+         bench_bool = True
+
+   for key,val in deadlift_dict.items():
+      if (val[0] == mov):
+         delete_date = key
+         deadlift_bool = True
+
+   if squat_bool:
+      del squat.get(current_user)[delete_date]
+   elif bench_bool:
+      del bench.get(current_user)[delete_date]
+      print(bench)
+   elif deadlift_bool:
+      del deadlift.get(current_user)[delete_date]
+   else:
+      pass
+
+def validateUser(username, password):
+   validate_pass = user_pass.get(username)
+   if validate_pass is not None:
+      if password == validate_pass:
+         current_user = username
+         return "true"
+   user_pass[username] = password
+   print(user_pass)
+   return "false"
+
+
+def printDeadliftJson():
+   y = json.dumps(deadlift.get(current_user))
+   return y
+
+
+def printSquatJson():
+   y = json.dumps(squat.get(current_user))
+   return y
+
+
+def printBenchJson():
+   y = json.dumps(bench.get(current_user))
+   return y
+
--- main.py
+++ main.py
@@ -1,20 +1,133 @@
-from flask import Flask, render_template
-from flask_bootstrap import Bootstrap
+#!/usr/bin/env python3
+from flask import Flask, render_template, jsonify,flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
+from app import *
+import os
 
 app = Flask(__name__)
-bootstrap = Bootstrap(app)
+
+UPLOAD_FOLDER = os.path.join(os.getcwd(), "static", "videos")
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+user = "demo"
 
 @app.route("/")
-def home():
-    return "Hello, World!"
+def login():
+    return render_template("login.html")
 
-@app.route("/jeffer")
-def salvador():
-    return "Hello, Jeffer"
- 
 @app.route("/home")
 def home_html():
     return render_template("home_page.html")
-   
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+
+@app.route("/squat")
+def squat():
+    return render_template("squat.html")
+
+
+@app.route("/bench")
+def bench():
+    print('hello')
+    return render_template("bench.html")
+
+
+@app.route("/deadlift")
+def deadlift():
+    return render_template("deadlift.html")
+
+
+@app.route("/squatupload")
+def squatupload():
+    return render_template("squat_upload.html")
+
+
+@app.route("/benchupload")
+def benchupload():
+    return render_template("bench_upload.html")
+
+
+@app.route("/deadliftupload")
+def deadliftupload():
+    return render_template("deadlift_upload.html")
+
+
+@app.route('/video/<videostr>')
+def video(videostr):
+    print(videostr)
+    vid_path = 'videos/' + videostr
+    return render_template("video.html", value=vid_path)
+
+
+@app.route('/deadlift_info', methods=['GET'])
+def get_deadlift_info():
+    return printDeadliftJson()
+
+
+@app.route('/squat_info', methods=['GET'])
+def get_squat_info():
+    return printSquatJson()
+
+
+@app.route('/bench_info', methods=['GET'])
+def get_bench_info():
+    return printBenchJson()
+
+
+@app.route('/logincred', methods=['GET'])
+def cred():
+    user = request.args.get('login')
+    password = request.args.get('password')
+    return validateUser(user, password)
+
+
+@app.route('/delete_entry', methods=['GET'])
+def delete():
+    print("called")
+    delVid = request.args.get('delete')
+    delete_vid(delVid)
+    return ""
+
+
+@app.route('/squat_upload_endpoint', methods = ['POST'])  
+def squat_upload_endpoint():  
+    try:
+        if request.method == 'POST':  
+            weight = request.values['weight']
+            rep = request.values['reps']
+            print(weight)
+            print(rep)
+            f = request.files['file']  
+            f.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(f.filename)))
+            return render_template("redirect_squat.html")  
+    except:
+        return render_template("redirect_bench.html") 
+
+
+@app.route('/bench_upload_endpoint', methods = ['POST'])  
+def bench_upload_enpoint():  
+    try:
+        if request.method == 'POST':  
+            f = request.files['file']  
+            f.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(f.filename)))
+            return render_template("redirect_bench.html") 
+    except:
+        return render_template("redirect_bench.html") 
+
+
+@app.route('/deadlift_upload_endpoint', methods = ['POST'])  
+def deadlift_upload_endpoint():  
+    try:
+        if request.method == 'POST':  
+            f = request.files['file']  
+            f.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(f.filename)))
+            return render_template("redirect_deadlift.html")  
+    except:
+        return render_template("redirect_bench.html") 
+
 if __name__ == "__main__":
     app.run(debug=True)
